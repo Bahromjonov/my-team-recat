fac39133c0618c826afe06ce0c4191b4ab627fa4
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-18 00:46:44 +0500

    a

---
+++ src/components/SomeOfOurClients.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// Imgs
+import bir from '../assets/1.png'
+import ikki from '../assets/2.svg'
+import uch from '../assets/3.svg'
+import tort from '../assets/4.svg'
+import besh from '../assets/5.svg'
+
+const SomeOfOurClients = () => {
+    return (
+        <section className='bg-sacramentoStateGreen'>
+            <div className='containerr py-36'>
+                <h2 className='main-still text-center mb-16'>Some of our clients</h2>
+                <div className='flex items-center justify-between'>
+
+              </div>
+            </div>
+        </section>
+    )
+}
+
+export default SomeOfOurClients
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutHero from '../components/AboutHero'
 import Directors from '../components/Directors'
+import SomeOfOurClients from '../components/SomeOfOurClients'
 const About = () => {
   return (
     <div>
       <AboutHero/>
       <Directors/>
+      <SomeOfOurClients/>
     </div>
   )
 }
