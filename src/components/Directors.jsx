124b16ab9e654799abf8f5710862196852f5dd41
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-17 16:22:22 +0500

    add directors container

---
+++ src/components/Directors.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Directors = () => {
+    return (
+        <section className='bg-deepJungleGreen py-40'>
+            <div className='containerr'>
+
+            </div>
+        </section>
+    )
+}
+
+export default Directors
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import AboutHero from '../components/AboutHero'
+import Directors from '../components/Directors'
 const About = () => {
   return (
     <div>
       <AboutHero/>
+      <Directors/>
     </div>
   )
 }
