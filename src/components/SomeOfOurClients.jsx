e0f5aaf214197207f62a68fc22d4a8a3c21fa676
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-18 09:33:11 +0500

    add SomeOfOurClients finished

--- src/components/SomeOfOurClients.jsx
+++ src/components/SomeOfOurClients.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 // Imgs
-import bir from '../assets/1.png'
-import ikki from '../assets/2.svg'
-import uch from '../assets/3.svg'
-import tort from '../assets/4.svg'
-import besh from '../assets/5.svg'
+import theVerage from '../assets/the-verge.svg'
+import theJakarta from '../assets/the-jakarta-post.svg'
+import theGuardian from '../assets/the-guardian.svg'
+import techradar from '../assets/techradar.svg'
+import gadgets from '../assets/gadgets-now.svg'
+import clientsImg from '../assets/clients-circle.svg'
 
 const SomeOfOurClients = () => {
     return (
         <section className='bg-sacramentoStateGreen'>
-            <div className='containerr py-36'>
+            <div className='containerr py-36 relative'>
+                <img className='absolute top-0 -left-32' src={clientsImg} alt="" aria-hidden='true' />
                 <h2 className='main-still text-center mb-16'>Some of our clients</h2>
-                <div className='flex items-center justify-between'>
-
-              </div>
+                <ul className='flex items-center justify-between'>
+                    <li>
+                        <img src={theVerage} alt="the verage" />
+                    </li>
+                    <li>
+                        <img src={theJakarta} alt="the Jakarta" />
+                    </li>
+                    <li>
+                        <img src={theGuardian} alt="the Guardian" />
+                    </li>
+                    <li>
+                        <img src={techradar} alt="techradar" />
+                    </li>
+                    <li>
+                        <img src={gadgets} alt="gadgets" />
+                    </li>
+                </ul>
             </div>
         </section>
     )
