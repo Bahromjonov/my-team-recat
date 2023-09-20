5d3f491026d08a7efa0b3ed437d174b2692160d7
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-20 14:11:32 +0500

    add contact finished

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -36,14 +36,21 @@ const Contact = () => {
         </div>
 
         <div className='w-full max-w-540 '>
-          <form action="https://echo.htmlacademy.ru" className=''>
+          <form action="https://echo.htmlacademy.ru">
             <div className=' '>
-              <input className='w-full bg-transparent  pl-3 border-b-2 pb-4 ' type="text" placeholder='Name'  required/>
-              <input className='w-full bg-transparent  pl-3 border-b-2 pb-4 pt-6 contact-email' type="email" placeholder='Name'  required/>
-              <div className='eror'>This field is required</div>
+              <input className='w-full bg-transparent text-still-base pl-3 border-b-2 pb-4 ' type="text" placeholder='Name' required />
+
+              <input className='w-full bg-transparent  text-still-base pl-3 border-b-2 pb-4 pt-6 contact-email' type="email" placeholder='Email Address' required />
+              <p className='eror font-Livvic'>This field is required</p>
               <span className='right'>Davom eting</span>
+
+              <input className='w-full text-still-base bg-transparent pt-6 pl-3 pb-4 border-b-2 ' type="text" placeholder='Company Name' required />
+              
+              <input className='w-full text-still-base bg-transparent pt-6 pl-3 pb-4 border-b-2 ' type="text" placeholder='Title' required />
+
+              <textarea  className='w-full min-h-[107px] bg-transparent  text-still-base pt-6 pl-3' name="Message" placeholder='Message'  required></textarea>
             </div>
-            <button>Submit</button>
+            <button className='py-2.5 px-8 header-link text-black bg-white rounded-full'>submit</button>
           </form>
         </div>
       </div>
